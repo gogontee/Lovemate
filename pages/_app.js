bb66d84a6e53d2411104b8bd2f8fa85c53ef5ab8
Simeon Great <stargatecares@gmail.com>
2025-07-15 11:55:05 +0100

    Fix casing issue with _app.js to resolve Vercel build error

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,6 @@ import "../styles/globals.css";
 // Inside your _app.js or _app.tsx
 import "react-phone-number-input/style.css";
 
-export default function App({ Component, pageProps }) {
+export default function app({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
